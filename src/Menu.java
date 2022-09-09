7de55f1d54a6e6db50c104457a7fe79fbe369b75
samuel <samuelsalcedoramirez@gmail.com>
2022-09-09 08:19:29 -0500

    Initial commit

---
+++ src/Menu.java
@@ -0,0 +1,16 @@
+import jdk.nashorn.internal.scripts.JO;
+
+import javax.swing.*;
+
+public class Menu {
+    public int opc() {
+
+        int opciones = Integer.parseInt(JOptionPane.showInputDialog("MENU\n" +
+                "1. Sumar\n" +
+                "2. Restar\n" +
+                "3. Dividir\n" +
+                "4. Salir"));
+        return opciones;
+
+    }
+}
